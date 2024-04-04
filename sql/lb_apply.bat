869c15aeb30a5e16907c8fc6702b708639e0f6d1
Vladimir Tischenko <vladimirtischenko82@gmail.com>
2024-04-04 10:56:55 +0300

    6_1_HW5_model_sql

---
+++ sql/lb_apply.bat
@@ -0,0 +1,8 @@
+set LB_HOME=C:\programs\liquibase-4.27.0
+call %LB_HOME%\liquibase.bat --driver=org.postgresql.Driver ^
+--classpath=%LB_HOME%\lib ^
+--changeLogFile=databaseChangeLog.sql ^
+--url="jdbc:postgresql://localhost:5432/masterjava" ^
+--username=user ^
+--password=password ^
+migrate
